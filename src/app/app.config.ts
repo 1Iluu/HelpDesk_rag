8606f731c47f84455757bab61626b4c472abc221
Jose Cardenas Coronado <88285865+1Iluu@users.noreply.github.com>
2025-11-18 00:22:22 -0500

    [Jose Cardenas]: Mejora
    Se modifico el formulario del feedback y se agrego la logica

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { provideHttpClient, withFetch} from '@angular/common/http';
+import { provideHttpClient, withFetch,withInterceptors} from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+import { authInterceptor } from './auth/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withFetch(),
+     withInterceptors([authInterceptor])
+  ),
   ]
 };
