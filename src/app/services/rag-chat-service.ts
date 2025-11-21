2556d1d943e5ad6e0ec5d805ecaaad416d3ab6b0
Jose Cardenas Coronado <88285865+1Iluu@users.noreply.github.com>
2025-11-21 01:01:48 -0500

    update url

--- src/app/services/rag-chat-service.ts
+++ src/app/services/rag-chat-service.ts
@@ -6,7 +6,7 @@ import { RagChunk } from '../models/rag-chunk';
 export class RagChatService {
 
   // 1. CAMBIO: Apuntamos directo a tu servidor Python manual
-  private pythonBaseUrl = 'http://localhost:8000';
+  private pythonBaseUrl = ' https://rag-agent-python-1097661750103.us-east1.run.app';
 
   private sessionId = signal<string | null>(null);
   private controller: AbortController | null = null;
